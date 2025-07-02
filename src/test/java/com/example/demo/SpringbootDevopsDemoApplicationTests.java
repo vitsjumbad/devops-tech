a0ce61743782c8695a472bb289aa0d879f56af2e
vitsjumbad <vitsjumbad@gmail.com>
2025-07-02 23:40:14 +0530

    add unit test

--- src/test/java/com/example/demo/SpringbootDevopsDemoApplicationTests.java
+++ src/test/java/com/example/demo/SpringbootDevopsDemoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringbootDevopsDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void greetEndpointReturnsExpectedMessage() {
+        String name = "TestUser";
+        String url = "http://localhost:" + port + "/greet?name=" + name;
+        String response = this.restTemplate.getForObject(url, String.class);
+        assertThat(response).isEqualTo("Hello, " + name + "!");
+    }
 }
